9076761b6530e3809366ac09b1603f4fa39f726f
Osman Said <57035358+Umid0vic@users.noreply.github.com>
2024-05-23 23:30:05 +0200

    Added TaskManger class

---
+++ TaskManager.cs
@@ -0,0 +1,31 @@
+﻿namespace ToDoReminder;
+
+public class TaskManager
+{
+    private List<Task> taskList;
+
+    public TaskManager()
+    {
+        taskList = new List<Task>();
+    }
+
+    public void AddTask(Task task)
+    {
+        taskList.Add(task);
+    }
+
+    public void ChangeTask(int index, Task task)
+    {
+        taskList[index] = task;
+    }
+
+    public void DeleteTask(int index)
+    {
+        taskList.RemoveAt(index);
+    }
+
+    public List<string> GetTasksAsString()
+    {
+        return taskList.Select(t => t.ToString()).ToList();
+    }
+}
